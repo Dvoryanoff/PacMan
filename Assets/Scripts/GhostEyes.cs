f80d45eae8b9589d4410f743afaf3a0b365195db
Dvoryanoff <dvoryanoff@mail.ru>
2022-06-03 19:42:30 +0300

    Add Files

---
+++ Assets/Scripts/GhostEyes.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GhostEyes : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Movement movement { get; private set; }
+    public Sprite up;
+    public Sprite down;
+    public Sprite left;
+    public Sprite right;
+
+    private void Awake()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.movement = GetComponentInParent<Movement>();
+    }
+
+    private void Update()
+    {
+        if (this.movement.direction == Vector2.up) {
+            this.spriteRenderer.sprite = up;
+        } else if (this.movement.direction == Vector2.down) {
+            this.spriteRenderer.sprite = down;
+        } else if (this.movement.direction == Vector2.left) {
+            this.spriteRenderer.sprite = left;
+        } else if (this.movement.direction == Vector2.right) {
+            this.spriteRenderer.sprite = right;
+        }
+    }
+}
